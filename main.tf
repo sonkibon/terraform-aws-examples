6d9cb2c96ad7d8d2c2e263e4695a20fe1ea04d8c
Son Kibon - 宋貴奉(そんきぼん) <son_kibon@cyberagent.co.jp>
2023-05-07 16:01:56 +0900

    chore: set a version constraint for required providers

--- main.tf
+++ main.tf
@@ -2,11 +2,15 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = " 3.74.2"
     }
     random = {
       source = "hashicorp/random"
+      version = ">= 2.1"
     }
   }
+
+  required_version = ">= 0.13"
 
   cloud {
     organization = "son-org"
