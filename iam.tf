da9b6946e1b9138e8bd7454cfce53f70349d9021
Son Kibon - 宋貴奉(そんきぼん) <son_kibon@cyberagent.co.jp>
2023-05-07 18:43:40 +0900

    feat: create iam role for glue resources

--- iam.tf
+++ iam.tf
@@ -44,3 +44,80 @@ data "aws_iam_policy_document" "read_s3_user" {
     ]
   }
 }
+
+resource "aws_iam_role" "glue" {
+  name = "glue-service-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "glue.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+data "aws_iam_policy_document" "glue" {
+  statement {
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.user.arn,
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.user.arn}/*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "glue:*",
+      "s3:GetBucketLocation",
+      "s3:ListBucket",
+      "s3:ListAllMyBuckets",
+      "s3:GetBucketAcl",
+      "iam:ListRolePolicies",
+      "iam:GetRole",
+      "iam:GetRolePolicy",
+      "cloudwatch:PutMetricData",
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams",
+      "logs:PutLogEvents",
+      "logs:PutRetentionPolicy",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "glue" {
+  name        = "${terraform.workspace}-${var.role}-glue-policy"
+  description = "Policies for AWS Glue crawler and job access to resources"
+  policy      = data.aws_iam_policy_document.glue.json
+}
+
+resource "aws_iam_role_policy_attachment" "glue" {
+  role       = aws_iam_role.glue.name
+  policy_arn = aws_iam_policy.glue.arn
+}
