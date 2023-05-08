229bc08bd522ad3c2a6fa305661c23cff227be0c
Son Kibon - 宋貴奉(そんきぼん) <son_kibon@cyberagent.co.jp>
2023-05-08 11:30:13 +0900

    feat: create glue job resources

--- glue.tf
+++ glue.tf
@@ -30,3 +30,128 @@ resource "aws_glue_crawler" "user_database_crawler" {
     ManagedBy   = "Terraform"
   }
 }
+
+resource "aws_glue_job" "export_processed_age_data_to_s3" {
+  name              = "export-processed-age-data-to-s3"
+  role_arn          = aws_iam_role.glue.arn
+  description       = "Process user's age data and upload to s3"
+  glue_version      = "4.0"
+  worker_type       = "G.1X"
+  number_of_workers = var.glue_job_export_processed_age_data_to_s3_number_of_workers
+
+  command {
+    script_location = "s3://${aws_s3_bucket.user.bucket}/job/export_processed_age_data_to_s3.py"
+    python_version  = "3"
+  }
+
+  default_arguments = {
+    "--ENV"                              = var.env
+    "--continuous-log-logGroup"          = aws_cloudwatch_log_group.glue_job.name
+    "--enable-continuous-cloudwatch-log" = "true"
+    "--enable-continuous-log-filter"     = "true"
+    "--enable-metrics"                   = "true"
+  }
+
+  tags = {
+    Name        = "export-processed-age-data-to-s3"
+    Environment = var.env
+    Application = "GlueJob"
+    Owner       = var.owner_tag
+    ManagedBy   = "Terraform"
+  }
+}
+
+resource "aws_glue_job" "export_processed_gender_data_to_s3" {
+  name              = "export-processed-gender-data-to-s3"
+  role_arn          = aws_iam_role.glue.arn
+  description       = "Process user's gender data and upload to s3"
+  glue_version      = "4.0"
+  worker_type       = "G.1X"
+  number_of_workers = var.glue_job_export_processed_gender_data_to_s3_number_of_workers
+
+  command {
+    script_location = "s3://${aws_s3_bucket.user.bucket}/job/export_processed_gender_data_to_s3.py"
+    python_version  = "3"
+  }
+
+  default_arguments = {
+    "--ENV"                              = var.env
+    "--continuous-log-logGroup"          = aws_cloudwatch_log_group.glue_job.name
+    "--enable-continuous-cloudwatch-log" = "true"
+    "--enable-continuous-log-filter"     = "true"
+    "--enable-metrics"                   = "true"
+  }
+
+  tags = {
+    Name        = "export-processed-gender-data-to-s3"
+    Environment = var.env
+    Application = "GlueJob"
+    Owner       = var.owner_tag
+    ManagedBy   = "Terraform"
+  }
+}
+
+resource "aws_glue_job" "export_processed_interests_data_to_s3" {
+  name              = "export-processed-interests-data-to-s3"
+  role_arn          = aws_iam_role.glue.arn
+  description       = "Generate interest data from user attribute information and upload to s3"
+  glue_version      = "2.0"
+  worker_type       = "G.2X"
+  number_of_workers = var.glue_job_export_processed_interests_data_to_s3_number_of_workers
+
+  command {
+    script_location = "s3://${aws_s3_bucket.user.bucket}/job/export_processed_interests_data_to_s3.py"
+    python_version  = "3"
+  }
+
+  default_arguments = {
+    "--ENV"                                     = var.env
+    "--continuous-log-logGroup"                 = aws_cloudwatch_log_group.glue_job.name
+    "--enable-continuous-cloudwatch-log"        = "true"
+    "--enable-continuous-log-filter"            = "true"
+    "--enable-metrics"                          = "true"
+    "--write-shuffle-files-to-s3"               = "true"
+    "--write-shuffle-spills-to-s3"              = "true"
+    "--conf spark.shuffle.glue.s3ShuffleBucket" = "s3://${aws_s3_bucket.user.bucket}"
+    "--TempDir"                                 = "s3://${aws_s3_bucket.user.bucket}/shuffle/"
+  }
+
+  tags = {
+    Name        = "export-processed-interests-data-to-s3"
+    Environment = var.env
+    Application = "GlueJob"
+    Owner       = var.owner_tag
+    ManagedBy   = "Terraform"
+  }
+}
+
+resource "aws_glue_job" "notify_workflow_status" {
+  name              = "notify-workflow-status"
+  role_arn          = aws_iam_role.glue.arn
+  description       = "Notify workflow status"
+  glue_version      = "4.0"
+  worker_type       = "G.1X"
+  number_of_workers = var.glue_job_notify_workflow_status_number_of_workers
+
+  command {
+    script_location = "s3://${aws_s3_bucket.user.bucket}/job/notify_workflow_status.py"
+    python_version  = "3"
+  }
+
+  default_arguments = {
+    "--ENV"                              = var.env
+    "--slack_webhook_url"                = var.glue_job_default_arguments_slack_webhook_url
+    "--continuous-log-logGroup"          = aws_cloudwatch_log_group.glue_job.name
+    "--enable-continuous-cloudwatch-log" = "true"
+    "--enable-continuous-log-filter"     = "true"
+    "--enable-metrics"                   = "true"
+  }
+
+  tags = {
+    Name        = "notify-workflow-status"
+    Environment = var.env
+    Application = "GlueJob"
+    Owner       = var.owner_tag
+    ManagedBy   = "Terraform"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -16,3 +16,27 @@ variable "owner_tag" {
 variable "glue_crawler_user_database_crawler_schedule" {
   default = "cron(30 21 * * ? *)"
 }
+
+variable "glue_trigger_export_member_age_gender_segment_to_s3_schedule" {
+  default = "cron(0 22 * * ? *)"
+}
+
+variable "glue_job_export_processed_age_data_to_s3_number_of_workers" {
+  default = 2
+}
+
+variable "glue_job_export_processed_gender_data_to_s3_number_of_workers" {
+  default = 5
+}
+
+variable "glue_job_export_processed_interests_data_to_s3_number_of_workers" {
+  default = 30
+}
+
+variable "glue_job_notify_workflow_status_number_of_workers" {
+  default = 2
+}
+
+variable "glue_job_default_arguments_slack_webhook_url" {
+  default = "https://hooks.slack.com/services/hoge/fuga/bar" # TODO: Replace this
+}
